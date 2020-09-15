1eabb519d765b994519610118aaa9ecba7c1e216
Eddie Haddican <edwardhaddican@gmail.com>
2020-09-15 10:04:12 -0400

    worked on the api call for all spells and for class spells

--- client/components/AllSpells.js
+++ client/components/AllSpells.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import Axios from 'axios'
+import SearchBar from './SearchBar'
 
 export default class AllSpells extends React.Component {
   constructor() {
     super()
     this.state = {
       allSpells: '',
-      error: ''
+      error: '',
+      classSpells: {}
     }
   }
   //i want all the spells to just load to this page when you open up the page
 
-  async fetchAllSpells(event) {
-    event.preventDefault()
+  componentDidMount() {
+    this.fetchAllSpells()
+    this.getClassSpells()
+  }
+
+  async fetchAllSpells() {
     try {
       let url = 'https://www.dnd5eapi.co/api/spells'
 
-      const results = await Axios.get(url)
+      const response = await Axios.get(url)
 
-      if (results.data.Error) {
+      if (response.data.Error) {
         this.setState({
           error: results.data.Error
         })
@@ -27,7 +33,7 @@ export default class AllSpells extends React.Component {
           error: ''
         })
         this.setState({
-          allSpells: results
+          allSpells: response.data.results
         })
 
         console.log('state', this.state.allSpells)
@@ -37,10 +43,38 @@ export default class AllSpells extends React.Component {
     }
   }
 
+  async getClassSpells() {
+    try {
+      let url = 'https://www.dnd5eapi.co'
+
+      const response = await Axios.get(url + '/api/classes')
+
+      const promises = response.data.results.map(element => {
+        return Axios.get(`${url}${element.url}/spells`)
+      })
+
+      const classSpellResults = await Promise.all(promises)
+
+      const classSpells = classSpellResults.reduce((result, element, index) => {
+        const playerClass = response.data.results[index].index
+        result[playerClass] = element.data.results
+        return result
+      }, {})
+
+      this.setState({
+        classSpells
+      })
+    } catch (error) {
+      console.log('You have an error', error)
+    }
+  }
+
   render() {
+    console.log('class Spells', this.state.classSpells)
     return (
       <div>
         <h1>All Spells</h1>
+        <SearchBar />
       </div>
     )
   }
---
+++ client/components/SearchBar.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import axios from 'axios'
+
+class SearchBar extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      search: '',
+      error: ''
+    }
+    this.fetchSpell = this.fetchSpell.bind(this)
+  }
+
+  searchBarUpdate(event) {
+    this.setState({
+      search: event.target.value
+    })
+  }
+
+  async fetchSpell(event) {
+    event.preventDefault()
+    try {
+      let url = 'https://www.dnd5eapi.co/api/spells'
+
+      if (this.state.search) {
+        url = url + `/${this.state.search}`
+      } else {
+        return
+      }
+
+      if (this.state.search) {
+        url = url + `&y=${this.state.year}`
+      }
+
+      const results = await axios.get(url)
+
+      if (results.data.Error) {
+        this.setState({
+          error: results.data.Error
+        })
+      } else {
+        this.setState({
+          error: ''
+        })
+        this.props.updateSearchResults(results.data.Search)
+      }
+    } catch (error) {
+      console.log('You have an error', error)
+    }
+  }
+
+  render() {
+    return (
+      <div className="searchbar-container">
+        <div>
+          <h2>Spell</h2>
+          {this.state.error ? (
+            <h3 className="banner-container">{this.state.error}</h3>
+          ) : null}
+        </div>
+        <div>
+          <form
+            onSubmit={this.fetchSpell}
+            className="searchbar-inner-container"
+          >
+            <div className="title-searchbar input-group">
+              <label>Spell:</label>
+              <input
+                value={this.state.search}
+                required
+                onChange={this.searchBarUpdate}
+              />
+            </div>
+            <button type="submit" className="search-button">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SearchBar
